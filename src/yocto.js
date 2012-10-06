9c474176c028b986c5b174c8c874400e095dcae0
Trygve Lie <github@trygve-lie.com>
2012-10-06 14:49:02 +0200

    Added tests for .put injection

--- src/yocto.js
+++ src/yocto.js
@@ -18,11 +18,13 @@
 		return value instanceof Object;
 	}
 
+
 	// Check if a value is an Function
 
 	function isFunction(value) { 
 		return typeof value === 'function';
 	}
+
 
 	// Check if a value is a Array
 
@@ -30,18 +32,143 @@
 		return value instanceof Array;
 	}
 
+
 	// Check if a value is a String
 
 	function isString(value) {
 		return typeof value === 'string';
 	}
 
-	// Array Remove - By John Resig (MIT Licensed)
-	// http://ejohn.org/blog/javascript-array-remove/
+
+	// Array Remove - 
+	// Source: John Resig - MIT Licensed - http://ejohn.org/blog/javascript-array-remove/
+
 	function arrayRemove(array, from, to) {
 		var rest = array.slice((to || from) + 1 || array.length);
 		array.length = from < 0 ? array.length + from : from;
 		return array.push.apply(array, rest);
+	}
+
+
+	// Object equality - Check if objects _looks_ alike. Ex: {foo: 'bar'} === {foo: 'bar'}
+	// Source: Underscore - DocumentCloud Licensed - http://underscorejs.org/
+
+	function isEqual(objA, objB) {
+
+		function eq(a, b, aStack, bStack) {
+			// Identical objects are equal. `0 === -0`, but they aren't identical.
+			// See the Harmony `egal` proposal: http://wiki.ecmascript.org/doku.php?id=harmony:egal.
+			if (a === b) {
+				return a !== 0 || 1 / a == 1 / b;
+			}
+		
+			// A strict comparison is necessary because `null == undefined`.
+			if (a == null || b == null) {
+				return a === b;
+			}
+
+			// Compare `[[Class]]` names.
+			var className = Object.prototype.toString.call(a);
+			if (className != Object.prototype.toString.call(b)) {
+				return false;	
+			}
+
+			switch (className) {
+				// Strings, numbers, dates, and booleans are compared by value.
+				case '[object String]':
+					// Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
+					// equivalent to `new String("5")`.
+					return a == String(b);
+				case '[object Number]':
+					// `NaN`s are equivalent, but non-reflexive. An `egal` comparison is performed for
+					// other numeric values.
+					return a != +a ? b != +b : (a == 0 ? 1 / a == 1 / b : a == +b);
+				case '[object Date]':
+				case '[object Boolean]':
+					// Coerce dates and booleans to numeric primitive values. Dates are compared by their
+					// millisecond representations. Note that invalid dates with millisecond representations
+					// of `NaN` are not equivalent.
+					return +a == +b;
+				// RegExps are compared by their source patterns and flags.
+				case '[object RegExp]':
+					return a.source == b.source &&
+						a.global == b.global &&
+						a.multiline == b.multiline &&
+						a.ignoreCase == b.ignoreCase;
+			}
+			
+			if (typeof a != 'object' || typeof b != 'object') {
+				return false;	
+			}
+
+			// Assume equality for cyclic structures. The algorithm for detecting cyclic
+			// structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
+			var length = aStack.length;
+			while (length--) {
+				// Linear search. Performance is inversely proportional to the number of
+				// unique nested structures.
+				if (aStack[length] == a) {
+					return bStack[length] == b;
+				} 
+			}
+			
+			// Add the first object to the stack of traversed objects.
+			aStack.push(a);
+			bStack.push(b);
+		
+			var size = 0, 
+				result = true;
+			// Recursively compare objects and arrays.
+			if (className == '[object Array]') {
+				// Compare array lengths to determine if a deep comparison is necessary.
+				size = a.length;
+				result = size == b.length;
+				if (result) {
+					// Deep compare the contents, ignoring non-numeric properties.
+					while (size--) {
+						if (!(result = eq(a[size], b[size], aStack, bStack))) break;
+					}
+				}
+			} else {
+				// Objects with different constructors are not equivalent, but `Object`s
+				// from different frames are.
+				var aCtor = a.constructor, bCtor = b.constructor;
+				if (aCtor !== bCtor && !(isFunction(aCtor) && (aCtor instanceof aCtor) &&
+										isFunction(bCtor) && (bCtor instanceof bCtor))) {
+					return false;
+				}
+				// Deep compare objects.
+				for (var key in a) {
+					if (hasOwnProperty.call(a, key)) {
+						// Count the expected number of properties.
+						size++;
+						// Deep compare each member.
+						if (!(result = hasOwnProperty.call(b, key) && eq(a[key], b[key], aStack, bStack))) {
+							break;
+						}
+					}
+				}
+				
+				// Ensure that both objects contain the same number of properties.
+				if (result) {
+					for (key in b) {
+						if (hasOwnProperty.call(b, key) && !(size--)) {
+							break;
+						} 
+					}
+					result = !size;
+				}
+			}
+			// Remove the first object from the stack of traversed objects.
+			aStack.pop();
+			bStack.pop();
+			return result;
+		}		
+
+		// Init recursion
+		return function init(a, b) {
+			return eq(a, b, [], []);
+		}(objA, objB);
 	}
 
 
@@ -67,6 +194,8 @@
 
 
 
+		// Drop all records in the database
+
 		drop : function() {			
 			arrayRemove(this.objects, 0, this.objects.length);
 			arrayRemove(this.next, 0, this.next.length);
@@ -74,6 +203,8 @@
 		},
 
 
+
+		// Put a single object or an array of objects into the database
 
 		put : function(obj, onSuccess) {
 			var success = false;
@@ -88,8 +219,8 @@
 				});
 
 				// Merge appended array into internal objects array.
-				this.objects	= this.objects.concat.apply(obj);
-				this.next		= this.next.concat.apply(obj);
+				this.objects	= this.objects.concat(obj);
+				this.next		= this.next.concat(obj);
 				success = !success;
 			}
 
@@ -111,10 +242,14 @@
 
 
 
+		// Get object(s) from the database based on a template object
+
 		get : function(template, onSuccess, time) {
 
+			var arr = (this.next.length === 0) ? 'objects' : 'next';
+
 			if (isObject(template) && !isArray(template)) {	
-				this.next = this[(this.next.length === 0) ? 'objects' : 'next'].filter(function(obj) {
+				this.next = this[arr].filter(function(obj) {
 					return Object.keys(template).every(function(key) {
 						return obj[key] === template[key];
 					});
@@ -139,10 +274,14 @@
 
 
 
+		// Sort a return from the database based on a objects property name
+
 		sort : function(key, onSuccess) {
 
+			var arr = (this.next.length === 0) ? 'objects' : 'next';
+
 			if (isString(key)) {
-				this.next = this[(this.next.length === 0) ? 'objects' : 'next'].sort(function(object1, object2) {
+				this.next = this[arr].sort(function(object1, object2) {
 					var key1 = '',
 						key2 = '';
 
@@ -174,8 +313,25 @@
 			}
 			
 			return this;
+		},
+
+
+
+		each : function(onEach) {
+			var arr	= (this.next.length === 0) ? 'objects' : 'next',
+				i	= 0,
+				l	= this[arr].length;
+
+			if (onEach && isFunction(onEach)) {
+				for (i = 0; i < l; i += 1) {					
+					onEach.call(this, this[arr][i]);
+				}
+			}
+
+			return this;
 		}
 
 	};
+
 
 })(typeof exports === 'undefined' ? this.yocto = {}: exports);
--- test/yocto.test.js
+++ test/yocto.test.js
@@ -4,10 +4,55 @@ var testCase = buster.testCase("yocto.js Test", {
 
     setUp: function() {
         this.db = yocto.db();
+        this.singleRecordA = {  id: "NOR_01359",
+                                lat: 63.68496,
+                                lng: 9.66531,
+                                name: "Brekstad Hurtigladestasjon, Ørland",
+                                street: "Landfestet",
+                                streetNumber: "1-5",
+                                points: 1,
+                                open24h: true,
+                                free: false,
+                                maxTime: 1
+                            };
+
+        this.singleRecordB = {  id: "NOR_00633",
+                                lat: 63.58871,
+                                lng: 10.74054,
+                                name: "Frostasenteret, Frosta",
+                                street: "Frostasenteret",
+                                streetNumber: "",
+                                points: 1,
+                                open24h: true,
+                                free: true,
+                                maxTime: 4
+                            };
+
+        this.arrayOfRecordsA = [
+            {id: "NOR_01338", lat: 59.92943, lng: 10.46686, name: "Coop Extra Rykkinn", street: "Valkyrieveien", streetNumber: "2", points: 1, open24h: true, free: true, maxTime: 6},
+            {id: "NOR_00481", lat: 63.41034, lng: 10.43997, name: "Moholt Storsenter, Trondheim", street: "Brøsetvegen", streetNumber: "177", points: 2, open24h: true, free : true, maxTime: 1},
+            {id: "NOR_00097", lat: 60.79564, lng: 11.08045, name: "Eidsiva Energi, Hamar", street: "Vangsvegen", streetNumber: "73", points: 2, open24h: false, free: true, maxTime: 2}
+        ];
+
+        this.arrayOfRecordsB = [
+            {id: "NOR_00725", lat: 60.15918, lng: 10.25634, name: "Eikliveien, Hønefoss", street: "Osloveien", streetNumber: "1", points: 4, open24h:true, free: true, maxTime: 8},
+            {id: "NOR_00039", lat: 60.46547, lng:  5.32418, name: "Åsane Storsenter, Bergen", street: "Åsane senter", streetNumber: "16", points: 2, open24h: false, free: true, maxTime: 1}
+
+        ];
+
+        this.arrayOfRecordsWithNonObjects = ['a', true, 42];
+
+        this.singleRecordNonObject = 42;
     },
 
     tearDown: function() {
         delete this.db;
+        delete this.singleRecordA;
+        delete this.singleRecordB;
+        delete this.arrayOfRecordsA;
+        delete this.arrayOfRecordsB;
+        delete this.arrayOfRecordsWithNonObjects;
+        delete this.singleRecordNonObject;
     },
 
 
@@ -16,31 +61,107 @@ var testCase = buster.testCase("yocto.js Test", {
     },
 
 
-    "test put of single object - should return an object array with the length 1": function() {
-        this.db.put({foo : 'bar'});
+    "test put of one single object and run callback - in callback _objects_ and _next_ should be 1, after execution _objects_ should be 1 and _next_ 0": function() {
+        var self = this;
+
+        this.db.put(this.singleRecordA, function(records) {
+            buster.assert.equals(self.db.objects.length, 1);
+            buster.assert.equals(self.db.next.length, 1);
+        });
+        
         buster.assert.equals(this.db.objects.length, 1);
+        buster.assert.equals(this.db.next.length, 0);
     },
  
-    "test put of non object - should not be inserted - should an object array with the length 0": function() {
-        this.db.put('fail');
-        buster.assert.equals(this.db.objects.length, 0);
+
+    "test put of multiple single objects and run callback - in callback _objects_ and _next_ should be 2, after execution _objects_ should be 2 and _next_ 0": function() {
+        var self = this;
+
+        this.db.put(this.singleRecordA).put(this.singleRecordB, function(records) {
+            buster.assert.equals(self.db.objects.length, 2);
+            buster.assert.equals(self.db.next.length, 2);
+        });
+        
+        buster.assert.equals(this.db.objects.length, 2);
+        buster.assert.equals(this.db.next.length, 0);
     },
 
-    "foo": function() {
-        this.db.put([{foo : 'bar'}], function(records){
-            buster.assert.equals(records.length, 1);
-        })
+
+    "test put of single array with objects and run callback - in callback _objects_ and _next_ should be 3, after execution _objects_ should be 3 and _next_ 0": function() {
+        var self = this;
+
+        this.db.put(this.arrayOfRecordsA, function(records) {
+            buster.assert.equals(self.db.objects.length, 3);
+            buster.assert.equals(self.db.next.length, 3);
+        });
+
+        buster.assert.equals(this.db.objects.length, 3);
+        buster.assert.equals(this.db.next.length, 0);
+    },
+
+
+    "test put of multiple arrays with objects and run callback - in callback _objects_ and _next_ should be 5, after execution _objects_ should be 5 and _next_ 0": function() {
+        var self = this;
+
+        this.db.put(this.arrayOfRecordsA).put(this.arrayOfRecordsB, function(records) {
+            buster.assert.equals(self.db.objects.length, 5);
+            buster.assert.equals(self.db.next.length, 5);
+        });
+
+        buster.assert.equals(this.db.objects.length, 5);
+        buster.assert.equals(this.db.next.length, 0);
+    },
+
+
+    "test put of one single object and not run callback - after execution _objects_ should be 1 and _next_ 1": function() {
+        var self = this;
+
+        this.db.put(this.singleRecordA);
+        
+        buster.assert.equals(this.db.objects.length, 1);
+        buster.assert.equals(this.db.next.length, 1);
+    },
+
+
+    "test put of single array with objects and not run callback - after execution _objects_ should be 3 and _next_ 3": function() {
+        var self = this;
+
+        this.db.put(this.arrayOfRecordsA);
+
+        buster.assert.equals(this.db.objects.length, 3);
+        buster.assert.equals(this.db.next.length, 3);
+    },
+
+
+    "test put of single non object - in callback and after execution _objects_ and _next_ should be 0": function() {
+        var self = this;
+
+        this.db.put(this.singleRecordNonObject, function(records){
+            buster.assert.equals(self.db.objects.length, 0);
+            buster.assert.equals(self.db.next.length, 0);
+        });
+
+        buster.assert.equals(this.db.objects.length, 0);
+        buster.assert.equals(this.db.next.length, 0);
+    },
+
+
+    "test put of single array with non object - in callback and after execution _objects_ and _next_ should be 0": function() {
+        var self = this;
+
+        this.db.put(this.arrayOfRecordsWithNonObjects, function(records){
+            buster.assert.equals(self.db.objects.length, 0);
+            buster.assert.equals(self.db.next.length, 0);
+        });
+
+        buster.assert.equals(this.db.objects.length, 0);
+        buster.assert.equals(this.db.next.length, 0);
     }
 
 
-// TESTS TO WRITE
-// - put - injection of single objects
-// - put - injection of array of objects
-// - put - appending single object to existing db
-// - put - appending array of objects to existing db
-// - put - appending array of objects where one entry is not an object (ex a string)
-// - put - appending array of objects where one entry is an array
 
+// TESTS TO WRITE
+// - all - misc form for chaining
 
 
 });
