e65f5b4cebe3a79508cac0571bb8dc780c8f0f0d
Trygve Lie <github@trygve-lie.com>
2012-10-07 18:30:53 +0200

    Added take method

--- src/yocto.js
+++ src/yocto.js
@@ -57,127 +57,6 @@
 	}
 
 
-	// Object equality - Check if objects _looks_ alike. Ex: {foo: 'bar'} === {foo: 'bar'}
-	// Source: Underscore - DocumentCloud Licensed - http://underscorejs.org/
-
-	function isEqual(objA, objB) {
-
-		function eq(a, b, aStack, bStack) {
-			// Identical objects are equal. `0 === -0`, but they aren't identical.
-			// See the Harmony `egal` proposal: http://wiki.ecmascript.org/doku.php?id=harmony:egal.
-			if (a === b) {
-				return a !== 0 || 1 / a == 1 / b;
-			}
-		
-			// A strict comparison is necessary because `null == undefined`.
-			if (a == null || b == null) {
-				return a === b;
-			}
-
-			// Compare `[[Class]]` names.
-			var className = Object.prototype.toString.call(a);
-			if (className != Object.prototype.toString.call(b)) {
-				return false;	
-			}
-
-			switch (className) {
-				// Strings, numbers, dates, and booleans are compared by value.
-				case '[object String]':
-					// Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
-					// equivalent to `new String("5")`.
-					return a == String(b);
-				case '[object Number]':
-					// `NaN`s are equivalent, but non-reflexive. An `egal` comparison is performed for
-					// other numeric values.
-					return a != +a ? b != +b : (a == 0 ? 1 / a == 1 / b : a == +b);
-				case '[object Date]':
-				case '[object Boolean]':
-					// Coerce dates and booleans to numeric primitive values. Dates are compared by their
-					// millisecond representations. Note that invalid dates with millisecond representations
-					// of `NaN` are not equivalent.
-					return +a == +b;
-				// RegExps are compared by their source patterns and flags.
-				case '[object RegExp]':
-					return a.source == b.source &&
-						a.global == b.global &&
-						a.multiline == b.multiline &&
-						a.ignoreCase == b.ignoreCase;
-			}
-			
-			if (typeof a != 'object' || typeof b != 'object') {
-				return false;	
-			}
-
-			// Assume equality for cyclic structures. The algorithm for detecting cyclic
-			// structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
-			var length = aStack.length;
-			while (length--) {
-				// Linear search. Performance is inversely proportional to the number of
-				// unique nested structures.
-				if (aStack[length] == a) {
-					return bStack[length] == b;
-				} 
-			}
-			
-			// Add the first object to the stack of traversed objects.
-			aStack.push(a);
-			bStack.push(b);
-		
-			var size = 0, 
-				result = true;
-			// Recursively compare objects and arrays.
-			if (className == '[object Array]') {
-				// Compare array lengths to determine if a deep comparison is necessary.
-				size = a.length;
-				result = size == b.length;
-				if (result) {
-					// Deep compare the contents, ignoring non-numeric properties.
-					while (size--) {
-						if (!(result = eq(a[size], b[size], aStack, bStack))) break;
-					}
-				}
-			} else {
-				// Objects with different constructors are not equivalent, but `Object`s
-				// from different frames are.
-				var aCtor = a.constructor, bCtor = b.constructor;
-				if (aCtor !== bCtor && !(isFunction(aCtor) && (aCtor instanceof aCtor) &&
-										isFunction(bCtor) && (bCtor instanceof bCtor))) {
-					return false;
-				}
-				// Deep compare objects.
-				for (var key in a) {
-					if (hasOwnProperty.call(a, key)) {
-						// Count the expected number of properties.
-						size++;
-						// Deep compare each member.
-						if (!(result = hasOwnProperty.call(b, key) && eq(a[key], b[key], aStack, bStack))) {
-							break;
-						}
-					}
-				}
-				
-				// Ensure that both objects contain the same number of properties.
-				if (result) {
-					for (key in b) {
-						if (hasOwnProperty.call(b, key) && !(size--)) {
-							break;
-						} 
-					}
-					result = !size;
-				}
-			}
-			// Remove the first object from the stack of traversed objects.
-			aStack.pop();
-			bStack.pop();
-			return result;
-		}		
-
-		// Init recursion
-		return function init(a, b) {
-			return eq(a, b, [], []);
-		}(objA, objB);
-	}
-
 
 // TODO: Add onLoad and onError functions to trigger when loading from localstorage
 	exports.db = function(config) {
@@ -301,8 +180,28 @@
 
 
 
-		take : function(template) {
+		// Takes matching objects out of the database
 
+		take : function(template, onSuccess) {
+
+			if (isObject(template) && !isArray(template)) {	
+				this.next = this.objects.filter(function(obj, index, orgArray) {					
+					return Object.keys(template).every(function(key) {
+						if (obj[key] === template[key]) {
+							arrayRemove(orgArray, index);
+							return true;
+						} else {
+							return false;
+						}
+					});
+				});
+			}
+
+			if (onSuccess && isFunction(onSuccess)) {
+				onSuccess.call(this, this.next);
+				arrayRemove(this.next, 0, this.next.length);
+			}
+			
 			return this;
 		},
 
