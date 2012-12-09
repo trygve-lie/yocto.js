51a9067abd278b4a48f2369609649f9d927601dc
Trygve Lie <github@trygve-lie.com>
2012-12-10 00:52:42 +0100

    Removed use of .every() - While loop is faster

--- src/es5.js
+++ src/es5.js
@@ -53,36 +53,6 @@ if (!Object.keys) {
 
 
 
-// ES5 shim for Array.every();
-// Source: https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/every
-
-if (!Array.prototype.every)
-{
-  Array.prototype.every = function(fun /*, thisp */)
-  {
-    "use strict";
- 
-    if (this == null)
-      throw new TypeError();
- 
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (typeof fun != "function")
-      throw new TypeError();
- 
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in t && !fun.call(thisp, t[i], i, t))
-        return false;
-    }
- 
-    return true;
-  };
-}
-
-
-
 // ES5 shim for Array.filter();
 // Source: https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/filter
 
