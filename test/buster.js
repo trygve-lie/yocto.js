c0ee315afa1c0cde7113007abacda0f257b6df27
Trygve Lie <github@trygve-lie.com>
2012-12-11 10:35:42 +0100

    Added package file and adjusted test config to be handled by npm test

--- test/buster.js
+++ test/buster.js
@@ -1,15 +1,15 @@
 var config = module.exports;
 
 config["Client tests"] = {
-    rootPath: "./",
+    rootPath: "../",
     environment: "browser",
     sources: [
-        "../src/es5.js",
-        "../src/yocto.js"
+        "src/es5.js",
+        "src/yocto.js"
     ],
     tests: [
-        "yocto.core.test.js",
-        "yocto.put.test.js",
-        "yocto.get.test.js"
+        "test/yocto.core.test.js",
+        "test/yocto.put.test.js",
+        "test/yocto.get.test.js"
     ]
 };
