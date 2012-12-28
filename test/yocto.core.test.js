e400194f56f4e8056c7ab34b28d2e67c255e59aa
Trygve Lie <github@trygve-lie.com>
2012-12-28 03:13:11 +0100

    Forcing Travis CI build :(

--- test/yocto.core.test.js
+++ test/yocto.core.test.js
@@ -16,6 +16,5 @@ var testCase = buster.testCase("yocto.js Test - Core", {
     // TODO: Add test for not beeing able to override name in config.
     // TODO: Test that object without matching uuid is not fucking up (type: not causing error due to key as ex obj.id missing)
     // TODO: Test for duplicte uuids. If config is _NOT_ provided duplicate uuids should be returned. If config is provided only a single uuid object should be returned
-    // Force build :/
 
 });
