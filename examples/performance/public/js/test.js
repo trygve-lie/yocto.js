84e94f21d53ca9634865b539fbc5e871853e17e5
Trygve Lie <github@trygve-lie.com>
2012-12-30 04:09:35 +0100

    #8 - Adding some performance tests

--- examples/performance/public/js/test.js
+++ examples/performance/public/js/test.js
@@ -1,70 +1,235 @@
-function perfTest(fn) {
-	var start = +new Date();
-	fn();
-	var end = +new Date();
-	return (end-start);
-}
+// Helper - Load data 
 
-
-function testGet() {
-	var result = perfTest(function(){
-		db.get({cc:"061",cn:"New York County"}, function(arr){
-		// db.get({code:19003}, function(arr){
-			console.log(arr);
-		});
-	});
-	document.querySelectorAll('#testGet .result')[0].innerHTML = result + 'ms';
-}
-
-
-function testGetEach() {
-	var result = perfTest(function(){
-		db.get({cc:"061",cn:"New York County"}).each(function(el){
-			console.log(el);
-		});
-	});
-	document.querySelectorAll('#testGetEach .result')[0].innerHTML = result + 'ms';
+function loadData(onLoaded){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/mock/data.json');
+  
+  xhr.onloadstart = function(e){
+    document.getElementById('info').innerHTML = 'Info: Loading objects from server';
+  }
+  
+  xhr.onloadend = function(e) {
+    var data = JSON.parse(this.response);
+    document.getElementById('info').innerHTML = 'Info: Loaded ' + data.codes.length + ' objects from server';
+    onLoaded.call(null, data);
+  }
+  
+  xhr.onerror = function(e) {
+    document.getElementById('info').innerHTML = 'Info: Loading objects from server failed!'; 
+  }
+  
+  xhr.send();
 }
 
 
 
-// Init a database
+// Test - Create database with no configuration
 
-var db = yocto.db();
+var database = function(ev) {
+
+  var el    = ev.target,
+      text  = el.innerHTML;
+
+  var bench = Benchmark(text, {
+    'fn' : function() {
+      var db = yocto.db();
+    },
+
+    'onStart' : function(event) {
+      el.setAttribute('class', 'running');
+      el.innerHTML = text + ' - Running';        
+    },
+    
+    'onComplete' : function(event) {
+      el.setAttribute('class', 'done');
+      el.innerHTML = String(event.target);
+      el.removeEventListener('click', database);
+    }
+  }).run({'async': true});
+
+}
+document.getElementById('db').addEventListener('click', database);
 
 
-// Load data from server
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '/mock/data.json');
-xhr.onload = function(e) {
-  var data = JSON.parse(this.response);
-  db.put(data.codes, function(arr){
-	document.getElementById('info').innerHTML = data.codes.length + ' objects loaded into database';
-	document.getElementById('testGet').addEventListener('click', testGet);
-	document.getElementById('testGetEach').addEventListener('click', testGetEach);
+// Test - Create database with no configuration and put() test data set into database
+
+var put = function(ev) {
+
+  var el    = ev.target,
+      text  = el.innerHTML;
+
+  loadData(function(objs){
+
+    var bench = Benchmark(text, {
+      'fn' : function() {
+        var db = yocto.db();        
+        db.put(objs.codes, function(objs){
+          // Do nothing. Just meachure speed
+        });
+      },
+
+      'onStart' : function(event) {
+        el.setAttribute('class', 'running');
+        el.innerHTML = text + ' - Running';        
+      },
+      
+      'onComplete' : function(event) {
+        el.setAttribute('class', 'done');
+        el.innerHTML = String(event.target);
+        el.removeEventListener('click', put);
+      }
+    }).run({'async': true});
+
   });
+
 }
-xhr.send();
+document.getElementById('put').addEventListener('click', put);
 
 
 
+// Test - Create database with uuid and put() test data set into database
 
+var put_uuid = function(ev) {
 
+  var el    = ev.target,
+      text  = el.innerHTML;
 
+  loadData(function(objs){
 
+    var bench = Benchmark(text, {
+      'fn' : function() {
+        var db = yocto.db({uuid:'code'});        
+        db.put(objs.codes, function(objs){
+          // Do nothing. Just meachure speed
+        });
+      },
 
+      'onStart' : function(event) {
+        el.setAttribute('class', 'running');
+        el.innerHTML = text + ' - Running';        
+      },
+      
+      'onComplete' : function(event) {
+        el.setAttribute('class', 'done');
+        el.innerHTML = String(event.target);
+        el.removeEventListener('click', put_uuid);
+      }
+    }).run({'async': true});
 
+  });
 
-function getTest() {
-	var start = +new Date(),
-		end = 0,
-		i = 100;
-
-	while(i--){
-		db.get({free:false}, function(arr){ });
-	}
-
-	end = +new Date();
-	console.log((end-start)/1000);
 }
+document.getElementById('put:uuid').addEventListener('click', put_uuid);
+
+
+
+// Test - Create database with uuid and put() test data set into database
+
+var put_timestamp = function(ev) {
+
+  var el    = ev.target,
+      text  = el.innerHTML;
+
+  loadData(function(objs){
+
+    var bench = Benchmark(text, {
+      'fn' : function() {
+        var db = yocto.db({timestamp:'timestamp'});        
+        db.put(objs.codes, function(objs){
+          // Do nothing. Just meachure speed
+        });
+      },
+
+      'onStart' : function(event) {
+        el.setAttribute('class', 'running');
+        el.innerHTML = text + ' - Running';        
+      },
+      
+      'onComplete' : function(event) {
+        el.setAttribute('class', 'done');
+        el.innerHTML = String(event.target);
+        el.removeEventListener('click', put_timestamp);
+      }
+    }).run({'async': true});
+
+  });
+
+}
+document.getElementById('put:timestamp').addEventListener('click', put_timestamp);
+
+
+
+// Test - get() with one object key in the template and no uuid enabled
+
+var get = function(ev) {
+
+  var db    = yocto.db(),
+      el    = ev.target,
+      text  = el.innerHTML;
+
+  loadData(function(objs){
+    db.put(objs.codes, function(){});
+
+    var bench = Benchmark(text, {
+      
+      'fn' : function() {
+        db.get({code:19003}, function(arr){
+          // Do nothing. Just meachure speed
+        });        
+      },
+      
+      'onStart' : function(event) {
+        el.setAttribute('class', 'running');
+        el.innerHTML = text + ' - Running';        
+      },
+      
+      'onComplete' : function(event) {
+        el.setAttribute('class', 'done');
+        el.innerHTML = String(event.target);
+        el.removeEventListener('click', get);
+      }
+    }).run({'async': true});
+
+  });
+
+}
+document.getElementById('get').addEventListener('click', get);
+
+
+
+// Test - get() with one object key in the template and with uuid enabled
+
+var get_uuid = function(ev) {
+
+  var db    = yocto.db({uuid:'code'}),
+      el    = ev.target,
+      text  = el.innerHTML;
+
+  loadData(function(objs){
+    db.put(objs.codes, function(){});
+
+    var bench = Benchmark(text, {
+      
+      'fn' : function() {
+        db.get({code:19003}, function(arr){
+          // Do nothing. Just meachure speed
+        });        
+      },
+      
+      'onStart' : function(event) {
+        el.setAttribute('class', 'running');
+        el.innerHTML = text + ' - Running';        
+      },
+      
+      'onComplete' : function(event) {
+        el.setAttribute('class', 'done');
+        el.innerHTML = String(event.target);
+        el.removeEventListener('click', get_uuid);        
+      }
+    }).run({'async': true});
+
+  });
+
+}
+document.getElementById('get:uuid').addEventListener('click', get_uuid);
